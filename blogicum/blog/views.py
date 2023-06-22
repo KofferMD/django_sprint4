e86f220977e018c170b9475866c0f39c8b79a02c
Your Name <b>
2023-06-22 20:53:48 +0300

    Fix flake8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -216,15 +216,3 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     def get_success_url(self):
         return reverse("blog:profile",
                        kwargs={"username": self.object.username})
-
-
-def page_not_found(request, exception):
-    return render(request, 'core/404.html', status=404)
-
-
-def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html', status=403)
-
-
-def internal_server_error(request):
-    return render(request, 'pages/500.html', status=500)
