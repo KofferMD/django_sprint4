c7a75d0d2756e8e9bd0cfa2a7424456351ece6d5
Винокуров Николай Андреевич <nvinokurov@sigma-it.ru>
2023-07-02 11:45:18 +0300

    Fix flake8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -49,14 +49,14 @@ class CommentMixin:
 class PostListView(PostMixin, ListView):
     def get_queryset(self):
         posts = Post.objects.prefetch_related(
-                    "author", "category", "location"
-                ).filter(
-                    pub_date__lt=dt.datetime.now(),
-                    is_published=True,
-                    category__is_published=True,
-                ).annotate(
-                    comment_count=Count("comment")
-                ).order_by("-pub_date")
+            "author", "category", "location"
+        ).filter(
+            pub_date__lt=dt.datetime.now(),
+            is_published=True,
+            category__is_published=True,
+        ).annotate(
+            comment_count=Count("comment")
+        ).order_by("-pub_date")
         return posts
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
